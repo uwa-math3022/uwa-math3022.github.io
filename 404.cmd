f9db02f60dd1892ff929938b3fb3f6dd08545fde
yawnoc <yawnoc@pm.me>
2020-07-22 07:41:30 +0800

    Add 404 page

---
+++ 404.cmd
@@ -0,0 +1,16 @@
+{+ resources/syntax.cmd +}
+
+%%
+  %title  Page not found
+  %date-modified  2020-07-22
+  \resources:no-maths
+%%
+
+# 404: %title #
+
+----
+**The requested page was not found.**
+----
+----
+Head back to the [home page](/).
+----
