123b0e5f82cc0f4c0fadb7556524ec1d5583af5a
yawnoc <yawnoc@pm.me>
2020-07-22 08:02:33 +0800

    Add syntax for maths (KaTeX) resources

--- resources/syntax.cmd
+++ resources/syntax.cmd
@@ -27,10 +27,34 @@ p{%%
 
 
 p{::
+  \resources:maths
+::
+  \\resources:no-maths
+  a~~
+    <script defer src="/resources/maths.min.js"></script>
+    <link rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css"
+      integrity="sha384-\
+        zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq\
+      "
+      crossorigin="anonymous">
+    <script defer
+      src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js"
+      integrity="sha384-\
+        y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz\
+      "
+      crossorigin="anonymous"
+      onload="renderMaths()"></script>
+  ~~
+::}
+
+
+p{::
   \resources:no-maths
 ::
-  %resources
+  %resources a~~
     <link rel="stylesheet" href="/resources/main.css">
+  ~~
 ::}
 
 
