7a1243cac60ae80a1a18fada4b0c9b6c9ed28d59
yawnoc <yawnoc@pm.me>
2020-07-29 13:49:19 +0800

    Prefer leading comma style for Mathematica
    'In general', use trailing comma between mandatory arguments
    and use a leading comma before an optional argument.

--- rod/index.cmd
+++ rod/index.cmd
@@ -788,8 +788,8 @@ FullSimplify[
   Divide[
     Integrate[-x Sin[n Pi x], {x, 0, 1}],
     Integrate[Sin[n Pi x] ^ 2, {x, 0, 1}]
-  ],
-  Element[n, Integers]
+  ]
+  , Element[n, Integers]
 ]
 ````
 ----
