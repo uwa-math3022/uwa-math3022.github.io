1f78e2fbbcb7b5ac2a4c936161e0da4095db517d
yawnoc <yawnoc@pm.me>
2020-08-08 00:17:15 +0800

    Slab example: add problem statement

--- slab/index.cmd
+++ slab/index.cmd
@@ -14,3 +14,31 @@
 ----
 **Your work doesn't need to be as detailed as this.**
 ----
+
+##{#problem} Problem ##
+
+$
+  \gdef \TI {T_\mathrm{I}}
+  \gdef \TE {T_\mathrm{E}}
+$
+
+----
+Consider a uniform slab of thickness $2L$, [thermal conductivity][k] $k$,
+[specific heat capacity][c] $c$, and [density][rho] $\rho$ .
+----
+----
+Suppose the rod is initially at temperature $\TI$ throughout,
+and thereafter loses heat to the environment at temperature $\TE$
+via [Newtonian cooling][newton] through its two faces,
+with a [heat transfer coefficient][h] of $h$.
+----
+----
+What happens?
+----
+
+@[k] https://en.wikipedia.org/wiki/Thermal_conductivity @
+@[c] https://en.wikipedia.org/wiki/Specific_heat_capacity @
+@[rho] https://en.wikipedia.org/wiki/Density @
+
+@[newton] https://en.wikipedia.org/wiki/Newton%27s_law_of_cooling @
+@[h] https://en.wikipedia.org/wiki/Heat_transfer_coefficient @
