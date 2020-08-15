078e4712321b87289276297e6b979a48f93c2ce7
yawnoc <yawnoc@pm.me>
2020-08-15 17:39:06 +0800

    Similarity: add problem statement

---
+++ similarity/index.cmd
@@ -0,0 +1,65 @@
+{+ resources/syntax.cmd +}
+
+%%
+  %title  Fundamental plane-source similarity solution
+  %date-modified  2020-08-xx
+  \resources:maths
+%%
+
+
+# %title #
+
+\noscript
+
+----
+**Your work doesn't need to be as detailed as this.**
+----
+
+----
+Source code for the images: [code.wl] \+
+\home
+----
+@[code.wl]
+  https://github.com/uwa-math3022/uwa-math3022.github.io/blob/master/\
+    plane/code.wl @
+
+##{#problem} Problem ##
+
+----
+Consider an infinite block of [density] $\rho$, [specific heat capacity] $c$,
+and [thermal conductivity] $k$.
+----
+----
+Suppose the block is initially at zero temperature throughout.
+At time $t = 0$:
+----
+====
+* Cram thermal energy per area $Q$ into the plane $x = 0$
+====
+----
+What happens?
+----
+
+@[density] https://en.wikipedia.org/wiki/Density @
+@[specific heat capacity]
+  https://en.wikipedia.org/wiki/Specific_heat_capacity @
+@[thermal conductivity] https://en.wikipedia.org/wiki/Thermal_conductivity @
+
+----{.qa}
+__Q.__
+  How can you put a non-zero amount of thermal energy
+  into a plane which has zero thickness?
+\+
+__A.__
+  It's an idealisation, like a point charge.
+  In the case of a point charge,
+  the charge density is infinite but the amount of charge is finite.
+  Here, the initial temperature of the plane $x = 0$ is infinite,
+  but the energy per area is finite.
+----
+
+\END
+
+----
+\home
+----
