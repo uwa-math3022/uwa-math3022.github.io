uwa-math3022/uwa-math3022.github.io

commit a61b657c17e5490126e9cb789f8615454d61bd35

Traffic: use horizontal PlotRange based on xMax

traffic/code.wl
@@ -118,9 +118,10 @@ timeStep = tMax / (tMaxInSeconds * framesPerSecond);
 (*Main options*)
 
 
+xMaxWithPadding = 1.1 xMax;
 mainOptions = {
   ImageSize -> 240,
-  PlotRange -> All,
+  PlotRange -> {{-xMaxWithPadding, xMaxWithPadding}, Automatic},
   {}
 };
 

commit 9f5aa7e28d74c37100272111536ddd61eb8d9a17

Traffic: fix cut-off x-axis label
caused by a61b657c.

traffic/code.wl
@@ -123,10 +123,9 @@ timePrePaddingProportion = 1/20;
 (*Main options*)
 
 
-xMaxWithPadding = 1.1 xMax;
 mainOptions = {
   ImageSize -> 240,
-  PlotRange -> {{-xMaxWithPadding, xMaxWithPadding}, Automatic},
+  PlotRange -> {{-1.1 xMax, 1.5 xMax}, Automatic},
   {}
 };
 